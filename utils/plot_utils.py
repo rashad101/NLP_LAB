94bf5e6427326cfcf07e7d6c6e7ee7370c1060ca
rashad101 <rah.rony@gmail.com>
2019-02-10 14:18:38 +0100

    File added

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,19 +1,5 @@
 from matplotlib import pyplot as plt
 
-def create_history_plot(history, model_name, metrics=None):
-    plt.title('Accuracy and Loss (' + model_name + ')')
-    if metrics is None:
-        metrics = {'acc', 'loss'}
-    if 'acc' in metrics:
-        plt.plot(history.history['acc'], color='g', label='Train Accuracy')
-        plt.plot(history.history['val_acc'], color='b', label='Validation Accuracy')
-    if 'loss' in metrics:
-        plt.plot(history.history['loss'], color='r', label='Train Loss')
-        plt.plot(history.history['val_loss'], color='m', label='Validation Loss')
-    plt.legend(loc='best')
-
-    plt.tight_layout()
-
 
 def create_history_plot_acc(history, model_name, metrics=None):
     plt.title('Accuracy (Relation Detector)')
@@ -38,12 +24,6 @@ def create_history_plot_loss(history, model_name, metrics=None):
     plt.tight_layout()
 
 
-def plot_history(history, model_name):
-    create_history_plot(history, model_name)
-    plt.show()
-
-
-
 def plot_loss(history,model_name,metrics):
     create_history_plot_loss(history,model_name,metrics)
     plt.savefig("img/loss.jpg")
@@ -54,7 +34,6 @@ def plot_and_save_history(history, model_name, file_path, metrics=None):
     if metrics is None:
         metrics = {'acc', 'loss'}
     create_history_plot_acc(history, model_name, metrics)
-    #create_history_plot_loss(history, model_name, metrics)
     plt.savefig("img/acc.jpg")
     plt.show()
     plot_loss(history,model_name,metrics=None)
